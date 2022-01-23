e99c348aea3e37a310bb4b3b665cbb4a59a1962a
Max Portador <deus7@protonmail.com>
2022-01-23 15:16:11 +0300

    initial

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+   return <header className='header'>
+        <img src="https://img.icons8.com/nolan/50/venn-diagram.png" alt='header logo'/>
+    </header>
+}
+
+export default Header;
