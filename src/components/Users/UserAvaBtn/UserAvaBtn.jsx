84a3b47d2a557f777083e82f9ee8792357e02668
Max Portador <deus7@protonmail.com>
2022-02-19 21:44:41 +0300

    lesson 55 done

--- src/components/Users/UserAvaBtn/UserAvaBtn.jsx
+++ src/components/Users/UserAvaBtn/UserAvaBtn.jsx
@@ -5,7 +5,7 @@ import css from "./UserAvaBtn.module.css";
 const UserAvaBtn = (props) => {
     return (
         <div className={css.ava_btn}>
-            <img className={css.img} src={props.img} alt="user ava"/>
+            <img className={css.img + " " + (!props.followed || css.followed)} src={props.img} alt="user ava"/>
             <UserFollowBtn followed={props.followed} follow={props.follow} unfollow={props.unfollow} id={props.id}/>
         </div>
     )
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,70 +6,40 @@ import {user_icon} from "../../redux/usersReducer";
 import css from "./Users.module.css"
 
 
-const Users = (props) => {
-    if (props.users.length === 0) {
 
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then( response => {
-            props.setUsers(response.data.items)
-        })
+class Users extends React.Component {
 
-        // props.setUsers(
-        //     [
-        //         {
-        //             id: 1,
-        //             followed: true,
-        //             img: user_icon,
-        //             fullName: 'Artyom',
-        //             status: 'I,m a boss',
-        //             location: {city: 'Samara', country: 'Russia'}
-        //         },
-        //         {
-        //             id: 2,
-        //             followed: true,
-        //             img: user_icon,
-        //             fullName: 'Maksim',
-        //             status: 'I,m a middle developer',
-        //             location: {city: 'Moscow', country: 'Russia'}
-        //         },
-        //         {
-        //             id: 3,
-        //             followed: false,
-        //             img: user_icon,
-        //             fullName: 'Svetlana',
-        //             status: 'I,m a model',
-        //             location: {city: 'Kiev', country: 'Ukraine'}
-        //         },
-        //         {
-        //             id: 4,
-        //             followed: false,
-        //             img: user_icon,
-        //             fullName: 'Anna',
-        //             status: 'I,m a coach',
-        //             location: {city: 'Saint Petersburg', country: 'Russia'}
-        //         },
-        //     ],
-        // )
-
+    componentDidMount() {
+        if (this.props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    this.props.setUsers(response.data.items)
+                })
+        }
     }
-    return (
-        <div className={css.users}>
-            {(props.users.map(user =>
-                    <div key={user.id}>
+
+    render() {
+        return (
+            <div className={css.users}>
+                {(this.props.users.map(user =>
+                        <div key={user.id}>
 
                             <span className={css.user}>
-                                <UserAvaBtn id={user.id} img={( props.users.photos  ? props.users.photos.small : user_icon)}
-                                            follow={props.follow}
+                                <UserAvaBtn id={user.id} img={( this.props.users.photos  ? this.props.users.photos.small : user_icon)}
+                                            follow={this.props.follow}
                                             followed={user.followed}
-                                            unfollow={props.unfollow}/>
+                                            unfollow={this.props.unfollow}/>
 
                                 <UserBoxInfo followed={user.followed} fullName={user.name}
                                              status={user.status} location={{city: "city", country: "country"}}/>
                             </span>
-                    </div>
-                )
-            )}
-        </div>
-    )
+                        </div>
+                    )
+                )}
+            </div>
+        )
+    }
+
 }
 
 export default Users;
