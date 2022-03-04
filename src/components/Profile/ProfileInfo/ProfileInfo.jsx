8d1a781abac520eac24266808dbeef58159d8d44
M.Shabanov <deus7@protonmail.com>
2022-03-04 16:57:21 +0300

    lesson 71 in progress

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import css from "./ProfileInfo.module.css";
 import {user_icon} from "../../../redux/usersReducer";
 import PreLoader from "../../PreLoader/PreLoader";
+import ProfileStatus from "./ProfileStatus/ProfileStatus";
 
 
 const ProfileInfo = (props) => {
@@ -14,33 +15,41 @@ const ProfileInfo = (props) => {
             <img src="https://static.orgpage.ru/socialnewsphotos/3c/3cc80415aa324fa2833df20a6aaf7e3a.jpg"
                  className={css.img} alt="Praha"/>
         </div>
-        {
-            props.profile
-                ?
-                <div className={css.description}>
-                    <img className={css.photo_large}
-                         src={props.profile.photos.large || user_icon}
-                         alt="фото пользователя"/>
+        { props.profile
+            ?   <div className={css.description}>
+                    <img className={css.photo_large} alt="фото пользователя"
+                        src={props.profile.photos.large || user_icon}/>
                     <div>{"Имя: " + profile.fullName}</div>
-                    {profile.aboutMe ? <div>
-                        <pre>{`${"О себе:".padEnd(10, " ")} ${profile.aboutMe}`}</pre>
-                    </div> : null}
-                    {contacts.length ? <div>{"Контакты"}</div> : null}
+                    {/*======================================= */}
+                    {profile.aboutMe 
+                    ?    <div>
+                            <pre>
+                                {`${"О себе:".padEnd(10, " ")} ${profile.aboutMe}`}
+                            </pre>
+                        </div> 
+                    :   null}
+                    {/*======================================= */}
+                    {contacts.length 
+                    ?   <div>{"Контакты"}</div> 
+                    :   null}
+                    {/*======================================= */}
                     <pre>
-                         {contacts.map( (val, id) =>
-                            <div key={id}>
-                                {`${val.padEnd(10, " ")}: ${profile.contacts[val]}` || null}
-                            </div>
+                        {contacts
+                        .map( (val, id) => <div key={id}>
+                                            { `${val.padEnd(10, " ")}: ${profile.contacts[val]}` || null }
+                                            </div>
                         )}
                     </pre>
-
+                    {/*======================================= */}
                     {profile.lookingForAJob && profile.lookingForAJobDescription
-                        ? <div>{profile.lookingForAJobDescription}</div>
-                        : null}
+                    ?   <div>{profile.lookingForAJobDescription}</div>
+                    :   null}
+                    
                 </div>
-                :
-                <PreLoader/>
+            :  <PreLoader/>
         }
+
+        <ProfileStatus status={"Hello my friends"}/>
     </div>
 }
 
