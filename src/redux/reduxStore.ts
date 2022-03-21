fda00fc9ce55448ab789cec82cbf1988a1a21120
M.Shabanov <deus7@protonmail.com>
2022-03-21 17:54:33 +0300

    Lesson 91 done

--- src/redux/reduxStore.ts
+++ src/redux/reduxStore.ts
@@ -15,6 +15,12 @@ let rootReducer = combineReducers({
     auth: authReducer,
     app: appReducer,
 })
+// @ts-ignore
+const composeEnhancers =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
 
-let store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+const enhancer = composeEnhancers(
+    applyMiddleware(thunkMiddleware)
+    // other store enhancers if any
+);
+let store = createStore(rootReducer, enhancer);
 export default store;
