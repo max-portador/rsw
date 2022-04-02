97b843b6aecb1fd0ac5b8a565b89f66c62342535
portador <deus7@protonmail.com>
2022-04-02 18:34:44 +0300

    Updates

---
+++ precache-manifest.8580598b870776de93d16df449941cd8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0173132b603f969e6360cb0bb5992b5f",
+    "url": "/rsw/index.html"
+  },
+  {
+    "revision": "82e0b3f04bf087d62d4a",
+    "url": "/rsw/static/css/3.048f1ffb.chunk.css"
+  },
+  {
+    "revision": "85e4e16de9787a819aa2",
+    "url": "/rsw/static/css/4.64363c30.chunk.css"
+  },
+  {
+    "revision": "63cf536d419a3d5e2fe3",
+    "url": "/rsw/static/css/5.64097718.chunk.css"
+  },
+  {
+    "revision": "a408162a5587cc44d1fe",
+    "url": "/rsw/static/css/main.7a8d9a91.chunk.css"
+  },
+  {
+    "revision": "8e37bd0fb572c8e2da93",
+    "url": "/rsw/static/js/2.eb6d30e1.chunk.js"
+  },
+  {
+    "revision": "82e0b3f04bf087d62d4a",
+    "url": "/rsw/static/js/3.4363555b.chunk.js"
+  },
+  {
+    "revision": "85e4e16de9787a819aa2",
+    "url": "/rsw/static/js/4.b410f87d.chunk.js"
+  },
+  {
+    "revision": "63cf536d419a3d5e2fe3",
+    "url": "/rsw/static/js/5.e96d4b71.chunk.js"
+  },
+  {
+    "revision": "f676bd591d8a4263bf05",
+    "url": "/rsw/static/js/6.85f68de3.chunk.js"
+  },
+  {
+    "revision": "a408162a5587cc44d1fe",
+    "url": "/rsw/static/js/main.952eda63.chunk.js"
+  },
+  {
+    "revision": "7cb8a6ed2eb1909cc856",
+    "url": "/rsw/static/js/runtime~main.f4c5b21b.js"
+  }
+]);
